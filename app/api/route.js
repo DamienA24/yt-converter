5531a325e063b504fc01b51f9164d6457765e900
DamienA24 <alves.d24@laposte.net>
2024-05-30 10:09:39 +0200

    feat(api): manage errors

--- app/api/route.js
+++ app/api/route.js
@@ -1,28 +1,46 @@
 import ytdl from "ytdl-core";
 import { getVideoMP3Base64 } from "yt-get";
+
 export async function POST(request) {
-  const { videoUrl, format } = await request.json();
+  try {
+    const { videoUrl, format } = await request.json();
 
-  if (ytdl.validateURL(videoUrl)) {
-    let data;
-    const info = await ytdl.getBasicInfo(videoUrl);
+    if (ytdl.validateURL(videoUrl)) {
+      let data;
+      const info = await ytdl.getBasicInfo(videoUrl);
 
-    if (format === "mp3") {
-      const { base64, title } = await getVideoMP3Base64(videoUrl);
-      data = Buffer.from(base64, "base64");
+      try {
+        if (format === "mp3") {
+          const { base64 } = await getVideoMP3Base64(videoUrl);
+          data = Buffer.from(base64, "base64");
+        } else {
+          data = ytdl(videoUrl);
+        }
+      } catch (conversionError) {
+        return new Response(
+          `Error converting video to ${format}: ${conversionError.message}`,
+          {
+            status: 500,
+          }
+        );
+      }
+
+      let titleVideo = info.videoDetails.title;
+      titleVideo = titleVideo.replace(/[^\x00-\x7F]/g, "");
+
+      return new Response(data, {
+        headers: {
+          "Content-Disposition": `attachment; filename="${titleVideo}.${format}"`,
+          "Content-Type": format === "mp3" ? "audio/mpeg" : "video/mp4",
+          "X-File-Name": `${titleVideo}.${format}`, // Custom header to include the file name
+        },
+      });
     } else {
-      data = ytdl(videoUrl);
+      return new Response("Invalid URL", { status: 400 });
     }
-
-    const titleVideo = info.videoDetails.title;
-
-    return new Response(data, {
-      headers: {
-        "Content-Disposition": `attachment; filename="${titleVideo}.${format}"`,
-        "Content-Type": format === "mp3" ? "audio/mpeg" : "video/mp4",
-        "X-File-Name": `${titleVideo}.${format}`, // Custom header to include the file name
-      },
+  } catch (error) {
+    return new Response(`Error processing request: ${error.message}`, {
+      status: 500,
     });
   }
-  return new Response("Invalid URL", { status: 400 });
 }
